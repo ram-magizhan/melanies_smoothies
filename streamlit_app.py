f41f3271a25a8a1f24177c2f09183cdfec64159f
ram-magizhan <ramkumar.sathiyanathan@gmail.com>
2025-09-01 21:00:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,9 @@ if ingredients_list:
         st.subheader(f+' Nutrition Information')
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == f, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', f,' is ', search_on, '.')
+        #st.write('The search value for ', f,' is ', search_on, '.')
         
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+f)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+sarch_on)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
